f4fd626f7fbee77c82fbbe6bfefe48857135fcbc
Mostafa Hassan <mo2stafa2002@gmail.com>
2023-10-27 14:14:06 +0200

    Add getAverage function

--- getSum.cpp
+++ getSum.cpp
@@ -11,9 +11,16 @@ int getSum(vector<int> v) {
     return sum;
 }
 
+double getAverage(const std::vector<int>& vec) {
+    int sum = getSum(vec);
+    return (double)sum / vec.size();
+}
+
+
 int main(){
     vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << "Sum: " << getSum(numbers) << endl;
+    cout << "Average: " << getAverage(numbers) << endl;
     return 0;
 }
 
